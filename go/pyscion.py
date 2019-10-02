62b68d03d87cb05dae7b0eb920e5544301397dc2
Juan A. García Pardo Giménez de los Galanes <juagargi@gmail.com>
2019-10-02 09:15:31 +0200

    API and sink (#2)
    * temp
    
    * WIP: getting paths
    
    * WIP start returning the paths
    
    * obtain paths
    
    * WIP open connection with path
    
    * Free mem in paths
    
    * open and close
    
    * WIP write
    
    * write call
    
    * listen, read
    
    * clean up
    
    * WIP sink
    
    * sink
    
    * add vendor and deps rule
    
    * cleanup a bit
    
    * rename viscon to go
    
    * SIGHUP and http POST makes sink dump and reset scores
    
    * Keep score per address, not only IA

---
+++ go/pyscion.py
@@ -0,0 +1,225 @@
+
+"""
+The API consists of:
+- init()
+- addr = local_address()
+- paths = paths(destination)
+- fd = open(destination, path)
+- close(fd)
+- write(fd, buffer)
+- fd = listen(port)
+- addr, n = read(fd, buffer)
+"""
+
+from ctypes import *
+from collections import namedtuple
+from ipaddress import IPv4Address
+
+
+lib = cdll.LoadLibrary('./scion_api.so')
+
+
+class SCIONException(Exception):
+    """ A generic error from SCION """
+    def __init__(self, error_msg):
+        if isinstance(error_msg, bytes):
+            error_msg = cstr_to_str(error_msg)
+        super().__init__(error_msg)
+
+charptr      = POINTER(c_char)
+
+def str_to_cstr(str):
+    return str.encode('utf-8')
+def cstr_to_str(cstr):
+    return cast(cstr, c_char_p).value.decode('utf-8')
+
+ # ---------------------------------------------------------
+
+class _PathInterface(Structure):
+    _fields_ = [('isdAs', c_char_p), 
+                ('ifid', c_size_t)]
+
+class _FwdPathMeta(Structure):
+    _fields_ = [('fwdPath_length', c_size_t),
+                ('fwdPath', POINTER(c_ubyte)),
+                ('mtu', c_ushort),
+                ('interfaces_length', c_size_t),
+                ('interfaces', POINTER(_PathInterface)),
+                ('expTime', c_uint)]
+
+class _HostInfo(Structure):
+    _fields_ =[('port', c_ushort),
+               ('ipv4', c_ubyte *4)]
+
+class _PathReplyEntry(Structure):
+    _fields_ = [('path', POINTER(_FwdPathMeta)),
+                ('hostInfo', _HostInfo)]
+
+
+ # ---------------------------------------------------------
+
+HostInfo = namedtuple('HostInfo', ['ipv4', 'port'])
+FwdPathMeta = namedtuple('FwdPathMeta', ['mtu', 'exp_time', 'fwd_path', 'interfaces'])
+Interface = namedtuple('Interface', ['isd_as', 'if_id'])
+
+
+class Path:
+    def __init__(self, cpath):
+        ip = IPv4Address(bytes(cpath.hostInfo.ipv4))
+        self.host_info = HostInfo(
+            ipv4=IPv4Address(bytes(cpath.hostInfo.ipv4)),
+            port=cpath.hostInfo.port
+        )
+        p = cpath.path.contents
+        interfaces = list(
+            Interface(
+                isd_as=cstr_to_str(p.interfaces[i].isdAs),
+                if_id=int(p.interfaces[i].ifid) )
+            for i in range(p.interfaces_length)
+        )
+        self.path = FwdPathMeta(
+            mtu=p.mtu,
+            exp_time=p.expTime,
+            fwd_path=bytes( (p.fwdPath[i] for i in range(p.fwdPath_length)) ),
+            interfaces=interfaces,
+        )
+
+    def __str__(self):
+        path = self.path.interfaces
+        return '{thisclass}({hostinfo}, {path})'.format(
+            thisclass=type(self).__name__,
+            hostinfo=str(self.host_info),
+            path=path)
+
+    def to_cstruct(self):
+        centry = _PathReplyEntry()
+        centry.hostInfo.port = self.host_info.port
+        centry.hostInfo.ipv4 = (c_ubyte*4).from_buffer_copy(self.host_info.ipv4.packed)
+
+        path = _FwdPathMeta()
+        path.mtu = self.path.mtu
+        path.expTime = self.path.exp_time
+        path.fwdPath_length = len(self.path.fwd_path)
+        path.fwdPath = POINTER(c_ubyte)()
+        path.fwdPath = cast(self.path.fwd_path, POINTER(c_ubyte))
+        path.interfaces_length = len(self.path.interfaces)
+        interfaces = (_PathInterface * len(self.path.interfaces))()
+        for i in range(len(self.path.interfaces)):
+            interfaces[i].ifid = self.path.interfaces[i].if_id
+            interfaces[i].isdAs = str_to_cstr(self.path.interfaces[i].isd_as)
+        path.interfaces = POINTER(_PathInterface)()
+        path.interfaces = interfaces
+
+        centry.path = POINTER(_FwdPathMeta)()
+        centry.path.contents = path
+        return centry
+
+
+class Paths:
+    """ A path from the source AS to the destination, as returned by SCION """
+    def __init__(self, paths, paths_len):
+        self._paths = [Path(paths[i]) for i in range(paths_len.value)]
+
+    def __str__(self):
+        return '\n'.join( ('[{i}] {p}'.format(
+            i=i,
+            p=self._paths[i]) for i in range(len(self._paths))) )
+
+    def __len__(self):
+        return len(self._paths)
+
+    def __getitem__(self, index):
+        return self._paths[index]
+
+    def __setitem__(self, index, value):
+        self._paths[index] = value
+
+    def __delitem__(self, index):
+        del self._paths[index]
+
+ # ---------------------------------------------------------
+
+
+lib.Init.restype = c_char_p
+def init():
+    err = lib.Init()
+    if err != None:
+        raise SCIONException(err)
+
+
+lib.LocalAddress.argtypes = [POINTER(charptr)]
+lib.LocalAddress.restype = c_char_p
+def local_address():
+    ptr = charptr()
+    err = lib.LocalAddress(byref(ptr))
+    if err != None:
+        raise SCIONException(err)
+    return cstr_to_str(ptr)
+
+
+lib.Paths.argtypes = [POINTER(c_size_t), POINTER(POINTER(_PathReplyEntry)), charptr]
+lib.Paths.restype = c_char_p
+lib.FreePathsMemory.argtypes = [POINTER(_PathReplyEntry), c_size_t]
+lib.FreePathsMemory.restype = c_char_p
+def paths(destination):
+    paths_n = c_size_t()
+    paths = (POINTER(_PathReplyEntry))()
+    err = lib.Paths(byref(paths_n), byref(paths), str_to_cstr(destination))
+    if err != None:
+        raise SCIONException(err)
+    pypaths = Paths(paths, paths_n)
+    err = lib.FreePathsMemory(paths, paths_n)
+    if err != None:
+        raise SCIONException(err)
+    return pypaths
+
+
+lib.Open.argtypes = [POINTER(c_long), charptr, POINTER(_PathReplyEntry)]
+lib.Open.restype = c_char_p
+def open(destination, path):
+    fd = c_long()
+    cpath = path.to_cstruct()
+    err = lib.Open(byref(fd), str_to_cstr(destination), cpath)
+    if err != None:
+        raise SCIONException(err)
+    return int(fd.value)
+
+
+lib.Close.argtypes = [c_long]
+lib.Close.restype = c_char_p
+def close(fd):
+    err = lib.Close(fd)
+    if err != None:
+        raise SCIONException(err)
+
+
+lib.Write.argtypes = [c_long, POINTER(c_char), c_size_t]
+lib.Write.restype = c_char_p
+def write(fd, buffer):
+    cbuffer = (c_char * len(buffer))(*buffer)
+    err = lib.Write(fd, cbuffer, len(buffer))
+    if err != None:
+        raise SCIONException(err)
+
+
+lib.Listen.argtypes = [POINTER(c_long), c_ushort]
+lib.Listen.restype = c_char_p
+def listen(port):
+    fd = c_long()
+    err = lib.Listen(byref(fd), c_ushort(port))
+    if err != None:
+        raise SCIONException(err)
+    return int(fd.value)
+
+
+lib.Read.argtypes = [POINTER(c_size_t), POINTER(charptr), c_long, POINTER(c_ubyte), c_size_t]
+lib.Read.restype = c_char_p
+def read(fd, buffer):
+    c_buffer = (c_ubyte * len(buffer)).from_buffer(buffer)
+    n = c_size_t()
+    client_address = charptr()
+    err = lib.Read(byref(n), byref(client_address), fd, c_buffer, len(buffer))
+    if err != None:
+        raise SCIONException(err)
+    return cstr_to_str(client_address), int(n.value)
+
---
+++ go/sink.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/netsec-ethz/scion-apps/lib/scionutil"
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/sciond"
+	"github.com/scionproto/scion/go/lib/snet"
+)
+
+// RECEPTION_BUFF_SIZE is the size of the buffer to read the packet from a client.
+const RECEPTION_BUFF_SIZE = 1024 * 65
+
+// PORT is the UDP port used to listen for incoming traffic.
+const PORT = 12345
+
+var localAddress *snet.Addr
+
+// scoreBoard is a map[addr.IA] = uint64
+var scoreBoard *sync.Map = new(sync.Map)
+
+type ScoreEntry struct {
+	Who        string
+	NumPackets uint64
+}
+
+func fatal(err error) {
+	panic(err)
+}
+func logError(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
+
+func initNetwork() {
+	localAddressStr, err := scionutil.GetLocalhostString()
+	if err != nil {
+		fatal(err)
+	}
+	localAddress, err = snet.AddrFromString(localAddressStr)
+	if err != nil {
+		fatal(err)
+	}
+	dispatcherPath := scionutil.GetDefaultDispatcher()
+	sciondPath := sciond.GetDefaultSCIONDPath(nil)
+	err = snet.Init(localAddress.IA, sciondPath, dispatcherPath)
+	if err != nil {
+		fatal(err)
+	}
+}
+
+func handleClients(conn snet.Conn) {
+	scoreChan := make(chan ScoreEntry)
+	go handleScore(scoreChan)
+	for {
+		buffer := make([]byte, RECEPTION_BUFF_SIZE)
+		n, clientAddr, err := conn.ReadFromSCION(buffer)
+		if err != nil {
+			logError("Error while reading from connection", err)
+			continue
+		}
+		go handleClient(conn, buffer[:n], clientAddr)
+		// keeping score here? Use a channel, to be prepared for when this is multi-threaded
+		addr := fmt.Sprintf("%s,[%v]", clientAddr.IA, clientAddr.Host.L3)
+		scoreChan <- ScoreEntry{
+			Who:        addr,
+			NumPackets: uint64(n),
+		}
+	}
+}
+
+// handleScore can handle the score board.
+func handleScore(scoreChan chan ScoreEntry) {
+	for {
+		e := <-scoreChan
+		board := scoreBoard
+		actual, found := board.Load(e.Who)
+		if !found {
+			actual = interface{}(uint64(0))
+		}
+		s := actual.(uint64)
+		s += e.NumPackets
+		board.Store(e.Who, s)
+	}
+}
+
+func handleClient(conn snet.Conn, buffer []byte, clientAddr *snet.Addr) {
+	sendAck(conn, clientAddr, uint16(len(buffer)))
+}
+
+func sendAck(conn snet.Conn, clientAddr *snet.Addr, message uint16) {
+	ackMessage := make([]byte, 2)
+	binary.LittleEndian.PutUint16(ackMessage, message)
+	_, err := conn.WriteTo(ackMessage, clientAddr)
+	if err != nil {
+		logError("Error sending ACK: %v", err)
+	}
+}
+
+func dumpScoreBoard() {
+	board := scoreBoard
+	scoreBoard = new(sync.Map)
+	fmt.Println("dumping score now")
+	fileName := "/tmp/scores.txt"
+	lines := make([]byte, 0)
+	board.Range(func(_addr, _n interface{}) bool {
+		addr := _addr.(string)
+		n := _n.(uint64)
+		lines = append(lines, ([]byte)(fmt.Sprintf("%s\t%d\n", addr, n))...)
+		return true
+	})
+	err := ioutil.WriteFile(fileName, lines, 0644)
+	if err != nil {
+		logError("Error writing scores: %v", err)
+	}
+}
+
+func initHTTP() {
+	http.HandleFunc("/", httpHandler)
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			fatal(err)
+		}
+	}()
+}
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		dumpScoreBoard()
+		w.WriteHeader(http.StatusOK)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func enableSigTermHandling() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGHUP)
+	go func() {
+		for {
+			<-signals
+			dumpScoreBoard()
+		}
+	}()
+}
+
+func main() {
+	initNetwork()
+	initHTTP()
+	localAddress.Host.L4 = addr.NewL4UDPInfo(PORT)
+	conn, err := snet.ListenSCION("udp4", localAddress)
+	if err != nil {
+		fatal(err)
+	}
+	enableSigTermHandling()
+	handleClients(conn)
+}
