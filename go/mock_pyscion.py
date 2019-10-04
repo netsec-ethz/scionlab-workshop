d01324f4aa2c0148ef65cb6b49b0921ef2c6786d
Juan A. Garcia Pardo <juagargi@gmail.com>
2019-10-04 14:54:33 +0200

    add mock API

---
+++ go/mock_pyscion.py
@@ -0,0 +1,63 @@
+
+"""
+The API consists of:
+- set_log_level(level)
+- init()
+- addr = local_address()
+- paths = paths(destination)
+- fd = connect(destination, path)
+- fd.close()
+- fd.write(buffer)
+- fd = listen(port)
+- addr, n = fd.read(buffer)
+"""
+
+
+
+class SCIONException(Exception):
+    pass
+
+
+class Path:
+    def __repr__(self):
+        return "PathMock()"
+
+
+class connect:
+    def __init__(self, destination, path):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        pass
+
+    def write(self, buffer):
+        pass
+
+    def read(self, buffer):
+        return '1-ffaa:ffff:ffff,[127.0.0.1]:0', 0
+
+    def close(self):
+        pass
+
+
+def set_log_level(level):
+    pass
+
+
+def init():
+    pass
+
+
+def local_address():
+    return '1-ffaa:0:0,[127.0.0.1]:<nil>'
+
+
+def paths(destination):
+    return [Path(), Path()]
+
+
+def listen(port):
+    return connect(None, None)
