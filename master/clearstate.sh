caf9bd95c4972d55890d66cf0e9a2cfda2c018c4
Kamila Souckova <coding@kamila.is>
2019-10-10 13:59:51 +0200

    also disable rounds when clearing state

--- master/clearstate.sh
+++ master/clearstate.sh
@@ -4,6 +4,7 @@ pipenv run buildbot stop buildbot-master
 
 echo '* Removing and re-creating the buildmaster database'
 rm -f buildbot-master/state.sqlite
+rm -f RUN
 pipenv run buildbot upgrade-master buildbot-master
 
 pipenv run buildbot start buildbot-master
